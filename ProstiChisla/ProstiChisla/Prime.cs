97c85f8860f1ee629b5873aa1508cc37b04c6afb
Gabrilita <96725781+Gabrilita@users.noreply.github.com>
2023-11-12 18:22:36 +0200

    Add files via upload

---
+++ ProstiChisla/ProstiChisla/Prime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstiChisla
+{
+    public class Prime
+    {
+        public bool Prosto(int num)
+        {
+            bool result = true;
+            for (int i = 2; i < num; i++)
+            {
+                if(num%i==0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ProstiChisla/ProstiChisla/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstiChisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi chislo");
+            int newNum = int.Parse(Console.ReadLine());
+
+            Prime prostoChislo= new Prime();
+            Console.WriteLine($"Chisloto {newNum} e prosto {prostoChislo.Prosto(newNum)}");
+        }
+    }
+}
